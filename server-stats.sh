c919fef42bbd99e9650251f68ce93356dfd9d979
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-04-13 18:14:47 -0500

    added disk usage

--- server-stats.sh
+++ server-stats.sh
@@ -10,17 +10,25 @@ echo "CPU Usage: $cpu"
 # Memory
 # Read memory from file
 
-read total available <<< $(awk '/MemTotal/ {t=$2} /MemAvailable/ {a=$2} END {print t, a}' /proc/meminfo)
+read total_memory available_memory <<< $(awk '/MemTotal/ {t=$2} /MemAvailable/ {a=$2} END {print t, a}' /proc/meminfo)
 
-used=$((total - available))
-used_percent=$(awk -v u=$used -v t=$total 'BEGIN { printf("%.1f", (u / t) * 100) }')
-free_percent=$(awk -v a=$available -v t=$total 'BEGIN { printf("%.1f", (a / t) * 100) }')
+used_memory=$((total_memory - available_memory))
+used_memory_percent=$(awk -v u=$used_memory -v t=$total_memory 'BEGIN { printf("%.1f", (u / t) * 100) }')
+free_memory_percent=$(awk -v a=$available_memory -v t=$total_memory 'BEGIN { printf("%.1f", (a / t) * 100) }')
 
 # Convert from kB to MB 
-total_mb=$(awk -v t=$total 'BEGIN { printf("%.1f", t/1024) }')
-used_mb=$(awk -v u=$used 'BEGIN { printf("%.1f", u/1024) }')
-available_mb=$(awk -v a=$available 'BEGIN { printf("%.1f", a/1024) }')
+total_memory_mb=$(awk -v t=$total_memory 'BEGIN { printf("%.1f", t/1024) }')
+used_mb=$(awk -v u=$used_memory 'BEGIN { printf("%.1f", u/1024) }')
+available_memory_mb=$(awk -v a=$available_memory 'BEGIN { printf("%.1f", a/1024) }')
 
-echo "Total Memory    : $total_mb MB"
-echo "Used Memory     : $used_mb MB ($used_percent%)"
-echo "Free/Available  : $available_mb MB ($free_percent%)"
+echo "Total Memory    : $total_memory_mb MB"
+echo "Used Memory     : $used_mb MB ($used_memory_percent%)"
+echo "Free/Available  : $available_memory_mb MB ($free_memory_percent%)"
+
+# Disk
+
+df_output=$(df -h /)
+
+size_disk=$(df_output | awk 'NR==2 {printf $2}')
+
+echo "Size : $size_disk"
