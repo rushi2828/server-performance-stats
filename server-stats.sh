e76ea8e16c4f7d9152ae702f9512af3ada9064a2
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-04-13 17:36:18 -0500

    added memory

--- server-stats.sh
+++ server-stats.sh
@@ -5,6 +5,17 @@ top_output=$(top -bn1)
 cpu=$( echo "$top_output" | grep "Cpu(s)" | sed 's/.*, *\([0-9.]*\)%* id.*/\1/' | awk '{print 100 - $1 "%"}')
 echo "CPU Usage: $cpu"
 
+read total available <<< $(awk '/MemTotal/ {t=$2} /MemAvailable/ {a=$2} END {print t, a}' /proc/meminfo)
 
-mem=$(awk '/MemTotal/ {total=$2} /MemAvailable/ {avail=$2} END {printf("%.1f%%", (1 - avail/total) * 100)}' /proc/meminfo)
-echo "Memory Usage: $mem"
+used=$((total - available))
+used_percent=$(awk -v u=$used -v t=$total 'BEGIN { printf("%.1f", (u / t) * 100) }')
+free_percent=$(awk -v a=$available -v t=$total 'BEGIN { printf("%.1f", (a / t) * 100) }')
+
+# Convert from kB to MB 
+total_mb=$(awk -v t=$total 'BEGIN { printf("%.1f", t/1024) }')
+used_mb=$(awk -v u=$used 'BEGIN { printf("%.1f", u/1024) }')
+available_mb=$(awk -v a=$available 'BEGIN { printf("%.1f", a/1024) }')
+
+echo "Total Memory    : $total_mb MB"
+echo "Used Memory     : $used_mb MB ($used_percent%)"
+echo "Free/Available  : $available_mb MB ($free_percent%)"
