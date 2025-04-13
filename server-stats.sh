c02c84a288120738137e37d3402837dd02dbcf45
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-04-13 17:11:46 -0500

    added mem usage

--- server-stats.sh
+++ server-stats.sh
@@ -4,3 +4,7 @@ top_output=$(top -bn1)
 
 cpu=$( echo "$top_output" | grep "Cpu(s)" | sed 's/.*, *\([0-9.]*\)%* id.*/\1/' | awk '{print 100 - $1 "%"}')
 echo "CPU Usage: $cpu"
+
+
+mem=$(awk '/MemTotal/ {total=$2} /MemAvailable/ {avail=$2} END {printf("%.1f%%", (1 - avail/total) * 100)}' /proc/meminfo)
+echo "Memory Usage: $mem"
