93d5153978a17d81078922dd2363e7f807171a6d
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-04-13 17:11:18 -0500

    edited top output

--- server-stats.sh
+++ server-stats.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-cpu=$(top -bn1 | grep "Cpu(s)" | sed 's/.*, *\([0-9.]*\)%* id.*/\1/' | awk '{print 100 - $1 "%"}')
+top_output=$(top -bn1)
+
+cpu=$( echo "$top_output" | grep "Cpu(s)" | sed 's/.*, *\([0-9.]*\)%* id.*/\1/' | awk '{print 100 - $1 "%"}')
 echo "CPU Usage: $cpu"
